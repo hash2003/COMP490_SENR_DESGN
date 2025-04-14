7d171a79b6026657514f690696c5017bf6f17990
jaimlich <81286832+jaimlich@users.noreply.github.com>
2025-04-14 12:45:38 -0700

    Delete frontend directory

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5001',  // This forwards all API requests to the backend server running on port 5001
-    },
-  },
-});
